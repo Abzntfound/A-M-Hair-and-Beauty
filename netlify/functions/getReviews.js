4a647132bf79b964e3cfa33318ef1445b7e81898
Abzntfound <adube6113@outlook.com>
2026-06-16 20:37:57 +0100

    Update getReviews.js

--- netlify/functions/getReviews.js
+++ netlify/functions/getReviews.js
@@ -1,18 +1,6 @@
-export async function handler(event) {
-  const body = JSON.parse(event.body);
-
-  const url = process.env.APPS_SCRIPT_URL;
-
-  const res = await fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body)
-  });
-
-  const data = await res.json();
-
+export async function handler() {
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: JSON.stringify({ ok: true })
   };
 }

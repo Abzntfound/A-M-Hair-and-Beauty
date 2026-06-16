f09e47fb33335b6594ae8f4d5f0b369b6d0f576a
Abzntfound <adube6113@outlook.com>
2026-06-16 20:44:23 +0100

    Update getReviews.js

--- netlify/functions/getReviews.js
+++ netlify/functions/getReviews.js
@@ -1,6 +1,23 @@
 export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ ok: true })
-  };
+  try {
+    const SHEET_ID = "12Q0Kp1-K4PnA5SsMQNovmcIAKdqVJsV_BKqItwMNFy4";
+    const SHEET_NAME = "Sheet1";
+    const API_KEY = process.env.SHEETS_API_KEY;
+
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}?key=${API_KEY}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.toString() })
+    };
+  }
 }
